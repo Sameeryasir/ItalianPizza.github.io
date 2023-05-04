428bbcd827ef079358e602b58abdea39c973332f
Sameeryasir <sameer02@gmail.com>
2023-05-04 20:28:57 +0500

    modified css html and javascript

--- food.js
+++ food.js
@@ -10,7 +10,8 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navbar.classList.remove ('open');
 };
-let darkmode=document.querySelector('#darkmode');
+
+let darkmode = document.querySelector('#darkmode');
 darkmode.onclick = () =>{
     if(darkmode.classList.contains('bx-moon' )){
         darkmode.classList.replace('bx-moon','bx-sun');
